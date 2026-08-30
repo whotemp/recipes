21093367dbeac4db90ecb49a02462bc69159cccd
Kevin Lang <kevin@whotemp.com>
2026-08-30 11:15:24 -0700

    update recipes

--- publish.sh
+++ publish.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-VAULT_RECIPES="/Users/kevinlang/Library/Mobile\ Documents/iCloud\~md\~obsidian/Documents/Kevin/Cooking/Recipes"
-
+VAULT_RECIPES="/Users/kevinlang/Library/Mobile Documents/iCloud~md~obsidian/Documents/Kevin/Cooking/Recipes"
 rsync -av --delete "$VAULT_RECIPES" "."
 git add -A && git commit -m "update recipes" && git push
